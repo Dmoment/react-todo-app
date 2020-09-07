d86634a8c79cf0c2c6bd7684416ce8abd22cfe45
Deepak Chauhan <rishudc119@gmail.com>
2020-09-07 19:35:22 +0530

    Unexpectedd token

--- app.js
+++ app.js
@@ -1,5 +1,130 @@
-ReactDOM.render(React.createElement(
-  "p",
-  null,
-  "Hello"
-), document.getElementById("root"));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Todo = function (_React$Component) {
+  _inherits(Todo, _React$Component);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    var _this = _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+
+    _this.state = { done: _this.props.done, text: _this.props.text };
+    _this.handleClick = _this.handleClick.bind(_this);
+    _this.handleChange = _this.handleChange.bind(_this);
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
+    return _this;
+  }
+
+  _createClass(Todo, [{
+    key: "handleClick",
+    value: function handleClick(event) {
+      this.setState(function (state) {
+        return {
+          done: !state.done
+        };
+      }, function (event) {
+        this.handleSubmit(event);
+      });
+    }
+  }, {
+    key: "handleChange",
+    value: function handleChange(event) {
+      var text = event.target.value;
+      this.setState(function (state) {
+        return { text: text };
+      });
+    }
+  }, {
+    key: "handleSubmit",
+    value: function handleSubmit(event) {
+      console.log("this is where submit will happen");
+    }
+  }, {
+    key: "render",
+    value: function render() {
+
+      return React.createElement(
+        "div",
+        { className: "todo" },
+        React.createElement(
+          "span",
+          null,
+          React.createElement("input", { type: "checkbox", checked: this.state.done,
+            onClick: this.handleClick }),
+          React.createElement("input", { type: "text", value: this.state.text,
+            className: this.state.done ? 'done' : 'not-done',
+            onChange: this.handleChange, onBlur: this.handleSubmit })
+        )
+      );
+    }
+  }]);
+
+  return Todo;
+}(React.Component);
+
+var TodoList = function (_React$Component2) {
+  _inherits(TodoList, _React$Component2);
+
+  function TodoList(props) {
+    _classCallCheck(this, TodoList);
+
+    var _this2 = _possibleConstructorReturn(this, (TodoList.__proto__ || Object.getPrototypeOf(TodoList)).call(this, props));
+
+    _this2.state = { todos: [{ _id: 'a',
+        text: 'Item1',
+        done: false }, { _id: 'b',
+        text: 'Item2',
+        done: false }] //This is an instance of the TodoList
+    };_this2.newTodo = _this2.newTodo.bind(_this2);
+    return _this2;
+  }
+
+  _createClass(TodoList, [{
+    key: "newTodo",
+    value: function newTodo(event) {
+      event.preventDefault(); //It cancels the default action belongs to the event in this case it cancels the null '#' link
+      todos = this.state.todos; //This has created object from instance of class TodoList
+      todos.push({ _id: "" }); //This will push a new entry to the local todos array. It won't change the original state of todos array
+      //now we have to change the original state of todos array
+      //this.state.todos = todos We cannot do this React doesn't allow this to happen. Reacts wants you to use special method to perform state change
+      //Therefore, state can only be changed by setState() method
+      this.setState(function (state) {
+        return {
+          todos: todos
+        };
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var todoList = this.state.todos.map(function (todo) {
+        return React.createElement(Todo, { key: todo._id.toString(), text: todo.text, done: todo.done });
+      });
+      return React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(
+          "h1",
+          null,
+          "React ToDo App"
+        ),
+        todoList,
+        React.createElement(
+          "a",
+          { href: "#", onClick: this.newTodo },
+          "New Todo"
+        )
+      );
+    }
+  }]);
+
+  return TodoList;
+}(React.Component);
+
+ReactDOM.render(React.createElement(TodoList, null), document.getElementById("root"));
--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,83 @@
-ReactDOM.render(<p>Hello</p>, document.getElementById("root"));
+class Todo extends React.Component {
+    constructor(props){ 
+        super(props);
+        this.state = {done: (this.props.done), text: (this.props.text)}
+        this.handleClick= this.handleClick.bind(this);
+        this.handleChange= this.handleChange.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    handleClick(event) {
+        this.setState(
+          state => ({
+            done: !state.done
+          }),
+          function(event){
+            this.handleSubmit(event);
+          }
+          
+        );
+      }
+
+    handleChange(event) {
+      let text= event.target.value;
+      this.setState(
+        state => ({text: text})
+        );
+    }
+
+    handleSubmit(event) {
+      console.log("this is where submit will happen");
+    }
+    render() {
+
+          return (
+              <div className="todo">
+                      <span>
+                            <input type="checkbox" checked={this.state.done}
+                            onClick={this.handleClick}></input>
+                            <input type="text" value={this.state.text}
+                            className={this.state.done ? 'done' : 'not-done'}
+                            onChange={this.handleChange} onBlur={this.handleSubmit}></input>
+                        </span>
+                      </div>);
+    }
+}
+
+class TodoList extends React.Component{
+  constructor(props){ 
+    super(props);
+    this.state = {todos: []} //This is an instance of the TodoList
+    this.newTodo = this.newTodo.bind(this);
+  }
+  
+
+  newTodo(event) {
+    event.preventDefault(); //It cancels the default action belongs to the event in this case it cancels the null '#' link
+    todos = this.state.todos; //This has created object from instance of class TodoList
+    todos.push({_id: ""});//This will push a new entry to the local todos array. It won't change the original state of todos array
+    //now we have to change the original state of todos array
+    //this.state.todos = todos We cannot do this React doesn't allow this to happen. Reacts wants you to use special method to perform state change
+   //Therefore, state can only be changed by setState() method
+   this.setState(state=>({
+     todos: todos
+   })
+   );
+
+  }
+
+  render(){ 
+    const todoList = this.state.todos.map((todo) =>
+                      <Todo key={todo._id.toString()} text={todo.text} done={todo.done} />
+    );
+    return <React.Fragment>
+            <h1>React ToDo App</h1>
+            {todoList}
+            <a href="#" onClick={this.newTodo}>New Todo</a>
+    </React.Fragment>
+  }
+
+}
+
+
+ReactDOM.render(<TodoList/>, document.getElementById("root"));
